832526b35cf95fda12be1ec5bd6d34ae5d2e6a56
Paperkeem <ahharlawhddl@gmail.com>
2023-01-17 02:57:48 +0900

    chore: setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CommentList from './components/CommentList';
+import Form from './components/Form';
+import PageList from './components/PageList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CommentList />
+      <PageList />
+      <Form />
     </div>
   );
 }
---
+++ src/components/PageList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function PageList() {
+  const pageArray = [];
+
+  pageArray.push(
+    // 임시로 페이지 하나만 설정했습니다.
+    <Page key="1">1</Page>
+  );
+
+  return <PageListStyle>{pageArray}</PageListStyle>;
+}
+
+const PageListStyle = styled.div`
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+const Page = styled.button`
+  padding: 0.375rem 0.75rem;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border: 1px solid lightgray;
+  margin-right: 3px;
+  
+  /* FIXME */
+  &:active{
+    background: gray;
+    color: #fff;
+  }
+`;
