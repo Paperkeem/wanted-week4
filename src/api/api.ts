6807005c46a9d4dd7cb4508642ca55c77f2aa085
Paperkeem <ahharlawhddl@gmail.com>
2023-01-17 12:18:45 +0900

    add: api logic

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,8 +7,8 @@ const instance = axios.create({
 
 export const callApi = {
   getList: () => instance.get(`/comments`),
-  addComment: (newComment: CommentState) => instance.post(`/comments`, newComment),
   getOneComment: (id: number) => instance.get(`/comments/${id}`),
+  addComment: (newComment: CommentState) => instance.post(`/comments`, newComment),
   updateComment: (id :number|undefined, newComment: CommentState) => instance.put(`/comments/${id}`, newComment),
   delComment: (id: number) => instance.delete(`/comments/${id}`),
 }
